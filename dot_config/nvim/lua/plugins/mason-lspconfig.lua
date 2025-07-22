cb5ab167e974a83acdb3ec9d73189b072e3613e1
Tomás Pinho <me@tomaspinho.com>
2025-07-22 12:52:56 +0100

    Prevent error on startup with mason-lspconfig

--- dot_config/nvim/lua/plugins/mason-lspconfig.lua
+++ dot_config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,10 @@
 return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
-    ensure_installed = { "postgres_lsp" },
+    -- Commented until mason-lspconfig is pinned in LazyVim to v2 which will
+    -- make it possible to install postgres_lsp
+
+    -- ensure_installed = { "postgres_lsp" },
   },
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
