e7bece0070d900ef1f84a24842bcf8321e2c08ad
Tomás Pinho <me@tomaspinho.com>
2026-03-07 22:00:10 +0000

    nightlight indicator: change colors

--- dot_config/waybar/executable_nightlight.sh
+++ dot_config/waybar/executable_nightlight.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "hyprsunset" >/dev/null; then
-  echo "{\"text\": \"󱩌\", \"tooltip\": \"Nightlight on\"}"
+  echo "{\"text\": \"󱩌\", \"tooltip\": \"Nightlight on\", \"class\": \"custom-nightlight-on\"}"
 else
-  echo "{\"text\": \"󱩍\", \"tooltip\": \"Nightlight off\"}"
+  echo "{\"text\": \"󱩍\", \"tooltip\": \"Nightlight off\", \"class\": \"custom-nightlight-off\"}"
 fi
