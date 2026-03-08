ce338ed7760466363954aa6750892808c645e7a7
Tomás Pinho <me@tomaspinho.com>
2026-03-08 21:04:51 +0000

    remove useless info, use icons only. better spacing overall, larger icon sizes

--- dot_config/waybar/executable_idlemanager.sh
+++ dot_config/waybar/executable_idlemanager.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "hypridle" >/dev/null; then
-  echo "{\"text\": \"󰒲 on \", \"tooltip\": \"Idle Management on\", \"class\": \"on\"}"
+  echo "{\"text\": \"󰒲\", \"tooltip\": \"Idle Management on\", \"class\": \"on\"}"
 else
-  echo "{\"text\": \"󰒳 off\", \"tooltip\": \"Idle Management off\", \"class\": \"off\"}"
+  echo "{\"text\": \"󰒳\", \"tooltip\": \"Idle Management off\", \"class\": \"off\"}"
 fi
--- dot_config/waybar/executable_nightlight.sh
+++ dot_config/waybar/executable_nightlight.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if pgrep -x "hyprsunset" >/dev/null; then
-  echo "{\"text\": \"󱩌 on \", \"tooltip\": \"Nightlight on\", \"class\": \"on\"}"
+  echo "{\"text\": \"󱩌\", \"tooltip\": \"Nightlight on\", \"class\": \"on\"}"
 else
-  echo "{\"text\": \"󱩍 off\", \"tooltip\": \"Nightlight off\", \"class\": \"off\"}"
+  echo "{\"text\": \"󱩍\", \"tooltip\": \"Nightlight off\", \"class\": \"off\"}"
 fi
